71b0f47aa4d9380f22565f668f653a2d266698be
Clark Roman <clarkroma4@gmail.com>
2019-11-15 15:57:20 -0600

    Added Caesar cipher code

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import 
+from caesar import rotate_string
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="POST">
+        <label for="rot"> Rotate By: </label>
+        <input type="text" name="rot" value="0"/>
+        <textarea name="text">Enter sentence to encrypt here! </textarea>
+        <input type="submit"/>
+        </form>
+    <body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+app.run()
